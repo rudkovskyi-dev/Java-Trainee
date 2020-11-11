7cee8477ff157454785812d3a0be822c28566f8e
rudkovskyi-dev <51890739+rudkovskyi-dev@users.noreply.github.com>
2020-11-11 19:57:21 +0200

    View update and StringConstants upload

---
+++ Java-Trainee/src/dev/rudkovskyi/MVC/view/StringConstant.java
@@ -0,0 +1,8 @@
+package dev.rudkovskyi.mvc.view;
+
+public interface StringConstant {
+    String ENTER_NAME = "enter.name";
+    String ENTER_NICKNAME = "enter.nickname";
+    String ENTER_PHONE = "enter.phone";
+    String WRONG_INFO = "wrong.info";
+}
--- Java-Trainee/src/dev/rudkovskyi/MVC/view/View.java
+++ Java-Trainee/src/dev/rudkovskyi/MVC/view/View.java
@@ -1,9 +1,59 @@
 package dev.rudkovskyi.mvc.view;
 
-public class View {
-    // Text's constants
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+import static dev.rudkovskyi.mvc.view.StringConstant.WRONG_INFO;
+
+public class View {
+
+    public static final ResourceBundle resourceBundle =
+            ResourceBundle.getBundle(
+                    "messages",
+                    new Locale("en")
+                    //new Locale("ua", "UA")
+            );
+
+    /**
+     * Prints the provided message
+     * @param message the string that will be printed
+     */
     public void printMessage(String message){
         System.out.println(message);
+    }
+
+    /**
+     * Concatenates several strings in one and returns the string result
+     * @param message one or more strings that should be concatenated
+     * @return returns the string result of concatenation
+     */
+    public String buildString(String... message){
+        StringBuilder strBuilder = new StringBuilder();
+        for (String item : message){
+            strBuilder.append(item);
+        }
+        return strBuilder.toString();
+    }
+
+    /**
+     * Prints provided message converting it to the currently set locale
+     * @param message the string from the resource bundle set
+     */
+    public void printInput(String message) {
+        printMessage(resourceBundle.getString(message));
+    }
+
+    /**
+     * Prints wrong information message and provided message
+     * converting them to the currently set locale
+     * @param message the string from the resource bundle set
+     */
+    public void printWrongInfo(String message){
+        printMessage(
+                buildString(
+                        resourceBundle.getString(WRONG_INFO),
+                        resourceBundle.getString(message)
+                )
+        );
     }
 }
